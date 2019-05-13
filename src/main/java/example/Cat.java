f5f384ec9ec2f06b514e0e7da1222a5cac13411a
d0409857@gmail.com <d0409857@gmail.com>
2019-05-13 09:49:30 +0800

    fff

---
+++ src/main/java/example/Cat.java
@@ -0,0 +1,13 @@
+package main.java.example;
+import java.util.logging.Logger;
+
+public class Cat {
+	
+  public void eat() {
+	  Logger.getLogger("ea eating.");
+  }
+
+  public void sound() {
+	  Logger.getLogger("Cat meow meow meow.");
+  }
+}
