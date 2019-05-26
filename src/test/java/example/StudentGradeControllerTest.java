ac6a659746b85f9e3284e18244bbe5c503f9c31f
d0409857@gmail.com <d0409857@gmail.com>
2019-05-26 18:55:16 +0800

    change

--- src/test/java/example/StudentGradeControllerTest.java
+++ src/test/java/example/StudentGradeControllerTest.java
@@ -6,8 +6,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
-
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -16,22 +14,22 @@ import main.java.example.StudentGradeController;
 public class StudentGradeControllerTest {
 	StudentGradeController s;
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() {
 		s=new StudentGradeController();
 	}
 
 
 	@Test
-	public void SortTest() {
-        LinkedHashMap<String, Integer> list=new LinkedHashMap<String, Integer>();
+	public void sortTest() {
+        LinkedHashMap<String, Integer> list=new LinkedHashMap<>();
 		
 		list.put("Tom", 72);
 		list.put("Jeff", 71);
 		list.put("Wendy", 70);
 	
-		ArrayList<Entry<String, Integer>> ls=new ArrayList<Entry<String, Integer>>();
+		ArrayList<Entry<String, Integer>> ls=new ArrayList<>();
 		ls.addAll(list.entrySet());
-		HashMap<String, Integer> Grades=new HashMap<String, Integer>();
+		HashMap<String, Integer> Grades=new HashMap<>();
 		Grades.put("Wendy", 70);
 		Grades.put("Tom", 72);
 		Grades.put("Jeff", 71);
