2adfca840a5585f8abe7bdf8f531be7d8399042c
d0409857@gmail.com <d0409857@gmail.com>
2019-05-26 16:24:33 +0800

    change

--- src/main/java/example/DataCompute.java
+++ src/main/java/example/DataCompute.java
@@ -1,22 +1,20 @@
 package main.java.example;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class DataCompute {
-	StudentGradeController S=new StudentGradeController();
+	StudentGradeController studentgradecontroller=new StudentGradeController();
 	
 	
 	public LinkedHashMap<String,String> ComputeVolunteer(LinkedHashMap<String,ArrayList<String>> Volunteers,LinkedHashMap<String, Integer> Grades,LinkedHashMap<String,Integer> AllSchoolGrade,
 			LinkedHashMap<String,Integer> AllSchoolQuota){
 		
-		ArrayList<Map.Entry<String,Integer>> list=S.Sort(Grades);
+		ArrayList<Map.Entry<String,Integer>> list=studentgradecontroller.Sort(Grades);
 		LinkedHashMap<String,String> StudentResult=new LinkedHashMap<String,String>();
 		
-		StudentResult=S.checkGrade(list, Volunteers, Grades, AllSchoolGrade, AllSchoolQuota);
+		StudentResult=studentgradecontroller.checkGrade(list, Volunteers, Grades, AllSchoolGrade, AllSchoolQuota);
 		return StudentResult;
 	
 	}
