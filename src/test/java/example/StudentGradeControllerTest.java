2aead566038bbb9e39d2f89613e206da34e0debd
d0409857@gmail.com <d0409857@gmail.com>
2019-05-26 18:14:53 +0800

    change

--- src/test/java/example/StudentGradeControllerTest.java
+++ src/test/java/example/StudentGradeControllerTest.java
@@ -7,37 +7,36 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import main.java.example.StudentGradeController;
 
 public class StudentGradeControllerTest {
-	StudentGradeController S;
+	StudentGradeController s;
 	@Before
-	public void setUp() throws Exception {
-		S=new StudentGradeController();
+	public void setUp(){
+		s=new StudentGradeController();
 	}
 
 
 	@Test
-	public void SortTest() {
-        LinkedHashMap<String, Integer> list=new LinkedHashMap<String, Integer>();
+	public void sortTest() {
+        LinkedHashMap<String, Integer> list=new LinkedHashMap<>();
 		
 		list.put("Tom", 72);
 		list.put("Jeff", 71);
 		list.put("Wendy", 70);
 	
-		ArrayList<Entry<String, Integer>> ls=new ArrayList<Entry<String, Integer>>();
+		ArrayList<Entry<String, Integer>> ls=new ArrayList<>();
 		ls.addAll(list.entrySet());
-		HashMap<String, Integer> Grades=new HashMap<String, Integer>();
-		Grades.put("Wendy", 70);
-		Grades.put("Tom", 72);
-		Grades.put("Jeff", 71);
+		HashMap<String, Integer> grades=new HashMap<>();
+		grades.put("Wendy", 70);
+		grades.put("Tom", 72);
+		grades.put("Jeff", 71);
 	
 	
-		assertEquals(ls,S.Sort(Grades));
+		assertEquals(ls,s.Sort(grades));
 	}
 
 }
