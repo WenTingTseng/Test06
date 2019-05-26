dc759c97cabbe870c8bca4af5a50a85ef7866b6b
d0409857@gmail.com <d0409857@gmail.com>
2019-05-26 16:32:52 +0800

    change

--- src/test/java/example/DataControllerTest.java
+++ src/test/java/example/DataControllerTest.java
@@ -58,9 +58,18 @@ public class DataControllerTest {
 		String[][] AllVolunteer= {{"台大","成大","中央"},{"成大","中興","中正"},{"中正","中山","逢甲"}};
 		String[] AllName= {"Wendy","Tony","Jeff"};
 		LinkedHashMap<String,ArrayList<String>> expected=new LinkedHashMap<String,ArrayList<String>>();
-		ArrayList<String> Wendy_ls = new ArrayList<String>(){{add("台大"); add("成大"); add("中央");}};
-		ArrayList<String> Tony_ls = new ArrayList<String>(){{add("成大"); add("中興"); add("中正");}};
-		ArrayList<String> Jeff_ls = new ArrayList<String>(){{add("中正"); add("中山");add("逢甲");}};
+		ArrayList<String> Wendy_ls = new ArrayList<String>();//{add("台大"); add("成大"); add("中央");}
+		Wendy_ls.add("台大");
+		Wendy_ls.add("成大");
+		Wendy_ls.add("中央");
+		ArrayList<String> Tony_ls = new ArrayList<String>();//{add("成大"); add("中興"); add("中正");}
+		Tony_ls.add("成大");
+		Tony_ls.add("中興");
+		Tony_ls.add("中正");
+		ArrayList<String> Jeff_ls = new ArrayList<String>();//{add("中正"); add("中山");add("逢甲");}
+		Jeff_ls.add("中正");
+		Jeff_ls.add("中山");
+		Jeff_ls.add("逢甲");
 		expected.put("Wendy",Wendy_ls);
 		expected.put("Tony",Tony_ls);
 		expected.put("Jeff",Jeff_ls);
