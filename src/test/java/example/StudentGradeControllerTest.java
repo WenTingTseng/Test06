5800fe8091763fdcc389f3cde24e433e50ea6ceb
d0409857@gmail.com <d0409857@gmail.com>
2019-06-04 14:24:31 +0800

    change

--- src/test/java/example/StudentGradeControllerTest.java
+++ src/test/java/example/StudentGradeControllerTest.java
@@ -18,9 +18,8 @@ public class StudentGradeControllerTest {
 		s=new StudentGradeController();
 	}
 
-
 	@Test
-	public void sortTest() {
+	public void sortTest1() {
         LinkedHashMap<String, Integer> list=new LinkedHashMap<>();
 		
 		list.put("Tom", 72);
@@ -33,9 +32,41 @@ public class StudentGradeControllerTest {
 		grades.put("Wendy", 70);
 		grades.put("Tom", 72);
 		grades.put("Jeff", 71);
-	
-	
+		
 		assertEquals(ls,s.theSort(grades));
 	}
+	@Test
+	public void sortTest2() {
+        LinkedHashMap<String, Integer> list=new LinkedHashMap<>();
+		
+		list.put("曾筱婷", 75);
+		list.put("王曉明", 64);
+		list.put("甘柔", 48);
+	
+		ArrayList<Entry<String, Integer>> ls=new ArrayList<>();
+		ls.addAll(list.entrySet());
+		HashMap<String, Integer> grades=new HashMap<>();
+		grades.put("王曉明", 64);
+		grades.put("曾筱婷", 75);
+		grades.put("甘柔", 48);
 
+		assertEquals(ls,s.theSort(grades));
+	}
+	@Test
+	public void sortTest3() {
+        LinkedHashMap<String, Integer> list=new LinkedHashMap<>();
+		
+		list.put("周湯豪", 68);
+		list.put("韓國瑜", 53);
+		list.put("李秉乾", 39);
+		
+		ArrayList<Entry<String, Integer>> ls=new ArrayList<>();
+		ls.addAll(list.entrySet());
+		HashMap<String, Integer> grades=new HashMap<>();
+		grades.put("李秉乾", 39);
+		grades.put("韓國瑜", 53);
+		grades.put("周湯豪", 68);
+
+		assertEquals(ls,s.theSort(grades));
+	}
 }
